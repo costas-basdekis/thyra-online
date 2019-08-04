125e47bc17e94eab7b936bfa120ea37cbc791a49
Costas Basdekis <costas.basdekis@gmail.com>
2019-08-04 21:15:34 +0100

    Publish site to GH pages

---
+++ docs/precache-manifest.87cd41ccd633492a2015baa328b48869.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a949baacc74eaab418fa46c97d9eeac",
+    "url": "/thyra-online/index.html"
+  },
+  {
+    "revision": "b9f747fa94a0f4267ff1",
+    "url": "/thyra-online/static/css/main.9b1bab0c.chunk.css"
+  },
+  {
+    "revision": "59d8df9bdc02cf4f4cc9",
+    "url": "/thyra-online/static/js/2.7b3a997b.chunk.js"
+  },
+  {
+    "revision": "b9f747fa94a0f4267ff1",
+    "url": "/thyra-online/static/js/main.ac16a160.chunk.js"
+  },
+  {
+    "revision": "7ea70767fbbb7c2259ba",
+    "url": "/thyra-online/static/js/runtime~main.966bc443.js"
+  }
+]);

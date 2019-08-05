62f842f9ea15144077a8f5b1ecf633df93bf1e8a
Costas Basdekis <costas.basdekis@gmail.com>
2019-08-06 00:37:07 +0100

    Add match making

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, {Component, Fragment} from 'react';
 import 'fomantic-ui-css/semantic.css';
-import {Button, Container, Header, Input, List, Segment, Tab} from 'semantic-ui-react';
+import {Button, Checkbox, Container, Header, Icon, Input, Label, List, Segment, Tab} from 'semantic-ui-react';
 import './styles/App.css';
 import Game from "./game/game";
 import Play from "./components/play";
@@ -12,26 +12,32 @@ class App extends Component {
     user: client.user,
     username: client.user ? client.user.name : null,
     users: client.users,
+    usersById: client.usersById,
+    games: client.games,
   };
 
   componentDidMount() {
     client.onUser = user => {
       this.setState({user, username: user ? user.name : null});
     };
-    client.onUsers = users => {
+    client.onUsers = (users, usersById) => {
       if (this.state.user) {
         const myIndex = users.findIndex(user => user.id === this.state.user.id);
         if (myIndex >= 0) {
           users = [users[myIndex], ...users.slice(0, myIndex), ...users.slice(myIndex + 1)];
         }
       }
-      this.setState({users: users});
+      this.setState({users, usersById});
+    };
+    client.onGames = games => {
+      this.setState({games});
     };
   }
 
   componentWillUnmount() {
     client.onUser = null;
     client.onUsers = null;
+    client.onGames = null;
   }
 
   makeMove = newGame => {
@@ -46,8 +52,12 @@ class App extends Component {
     client.changeUsername(this.state.username);
   };
 
+  changeReadyToPlay = (e, {checked}) => {
+    client.changeReadyToPlay(checked);
+  };
+
   render() {
-    const {game, user, username, users} = this.state;
+    const {game, user, username, users, usersById, games} = this.state;
     return (
       <Container text>
         <Segment textAlign={"center"}>
@@ -61,11 +71,32 @@ class App extends Component {
                   Welcome
                   <Input value={username} onChange={this.changeUsername} />
                   <Button onClick={this.updateUsername}>Change</Button>
-                  <List bulleted>
-                    {users.map(otherUser => (
-                      <List.Item key={otherUser.id}>{otherUser.name} {otherUser.id === user.id ? "(me)" : null}</List.Item>
-                    ))}
-                  </List>
+                  <br />
+                  <Checkbox label={"Ready to play?"} checked={user.readyToPlay} onChange={this.changeReadyToPlay} />
+                  <Tab menu={{pointing: true}} panes={[
+                    {menuItem: `${users.length} users`, render: () => (
+                      <List bulleted>
+                        {users.map(otherUser => (
+                          <List.Item key={otherUser.id}>
+                            {otherUser.name}
+                            {otherUser.id === user.id ? <Label><Icon name={"user"} />Me</Label> : null}
+                            {otherUser.readyToPlay ? <Label><Icon name={"checkmark"} color={"green"} />Ready to play</Label> : null}
+                          </List.Item>
+                        ))}
+                      </List>
+                    )},
+                    {menuItem: `${games.length} games`, render: () => (
+                        <List bulleted>
+                          {games.map(game => (
+                            <List.Item key={game.id}>
+                              {usersById[game.userIds[0]].name} {usersById[game.userIds[0]].id === user.id ? <Label><Icon name={"user"} />Me</Label> : null}
+                              {" vs "}
+                              {usersById[game.userIds[1]].name}
+                            </List.Item>
+                          ))}
+                        </List>
+                    )}
+                  ]} />
                 </Fragment>
               ) : "Connecting to server..."}
             </Tab.Pane>
--- src/client/client.js
+++ src/client/client.js
@@ -1,6 +1,11 @@
 class Client {
   constructor() {
+    this.id = localStorage.getItem('user-id') || null;
+    this.password = localStorage.getItem('user-password') || null;
     this.socket = window.io("http://localhost:4000");
+    this.socket.on('connect', () => {
+      this.getUser();
+    });
     this.socket.on('disconnect', () => {
       this.gotUser(null);
       this.gotUsers([]);
@@ -11,17 +16,28 @@ class Client {
     this.socket.on("user", this.gotUser);
 
     this.users = [];
+    this.usersById = {};
     this.onUsers = null;
     this.socket.on("users", this.gotUsers);
+
+    this.games = [];
+    this.onGames = null;
+    this.socket.on("games", this.gotGames);
 
     this.getUser();
   }
 
   getUser() {
-    this.socket.emit("create-user");
+    this.socket.emit("create-user", {id: this.id, password: this.password});
   }
 
   gotUser = user => {
+    if (user) {
+      this.id = user.id;
+      this.password = user.password;
+      localStorage.setItem('user-id', this.id);
+      localStorage.setItem('user-password', this.password);
+    }
     this.user = user;
     if (this.onUser) {
       this.onUser(user);
@@ -32,10 +48,25 @@ class Client {
     this.socket.emit("change-username", name);
   }
 
+  changeReadyToPlay(checked) {
+    this.socket.emit("change-ready-to-play", !!checked);
+  }
+
   gotUsers = users => {
     this.users = users;
+    this.usersById = {};
+    for (const user of this.users) {
+      this.usersById[user.id] = user;
+    }
     if (this.onUsers) {
-      this.onUsers(users);
+      this.onUsers(users, this.usersById);
+    }
+  };
+
+  gotGames = games => {
+    this.games = games;
+    if (this.onGames) {
+      this.onGames(games);
     }
   };
 }

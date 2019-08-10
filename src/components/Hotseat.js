ee6c089e2248f2158bd9b357b21f3e967269d8a4
Costas Basdekis <costas.basdekis@gmail.com>
2019-08-10 22:19:27 +0100

    Add share hotseat game link

--- src/components/Hotseat.js
+++ src/components/Hotseat.js
@@ -1,20 +1,95 @@
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
 import Play from "./Play";
 import Game from "../game/game";
+import {Button, Icon, Grid, Modal} from "semantic-ui-react";
+import * as utils from "../utils";
 
 class Hotseat extends Component {
   state = {
-    game: Game.create(),
+    error: false,
+    ...this.constructor.getGameAndErrorFromUrlPosition(),
+  };
+
+  static getGameAndErrorFromUrlPosition() {
+    const params = new URLSearchParams(window.location.search);
+    const position = params.get('position');
+    if (position) {
+      let game, error;
+      try {
+        game = Game.fromCompressedNotation(position);
+        if (!game) {
+          error = 'The position was not valid';
+        }
+      } catch (e) {
+        game = null;
+        error = 'The series of moves where not valid'
+      }
+      if (error) {
+        return {game: Game.create(), error};
+      }
+      return {game, error: false};
+    } else {
+      return {game: Game.create(), error: false};
+    }
+  }
+
+  dismissUrlPositionError = () =>{
+    this.setState({error: null});
   };
 
   makeMove = newGame => {
     this.setState({game: newGame});
   };
 
+  share = () => {
+    const url = this.props.location.pathname;
+    if (navigator.share) {
+      const game = this.state.game;
+      navigator.share({
+        title: `Thyra Online - Game after ${game.moveCount} moves`,
+        text: `Review Santorini game between after ${game.moveCount} moves`,
+        url,
+      }).catch(() => {
+        utils.copyToClipboard(url);
+        alert('Link copied to clipboard');
+      });
+    } else {
+      utils.copyToClipboard(url);
+      alert('Link copied to clipboard');
+    }
+  };
+
   render() {
-    const {game} = this.state;
+    const {game, error} = this.state;
+
     return (
-      <Play game={game} makeMove={this.makeMove} />
+      <Fragment>
+        <Modal
+          open={error}
+          size={'mini'}
+          onClose={this.dismissUrlPositionError}
+          header={'Could not load shared position'}
+          content={`${error}. Please check that you copied the full URL`}
+          actions={[{content: 'OK', positive: true}]}
+        />
+        <Grid columns={'equal'}>
+          <Grid.Column />
+          <Grid.Column>
+            <Button
+              positive
+              icon
+              onClick={this.share}
+              style={{width: '100%'}}
+              as={'a'}
+              href={`/hotseat?position=${game.compressedFullNotation}`}
+              title={navigator.share ? 'Click to open the sharing menu' : 'Click to copy URL to game'}
+            >
+              <Icon name={'share'}/> Share position
+            </Button>
+          </Grid.Column>
+        </Grid>
+        <Play game={game} makeMove={this.makeMove} />
+      </Fragment>
     );
   }
 }

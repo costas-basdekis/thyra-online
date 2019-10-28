badbbcd68aabe94f275010811769909de2d163a9
Costas Basdekis <costas.basdekis@gmail.com>
2019-10-28 09:12:34 +0000

    Bump client version

--- src/client/client.js
+++ src/client/client.js
@@ -2,7 +2,7 @@ import _ from "lodash";
 import moment from "moment";
 import * as utils from "../utils";
 
-const appVersion = 8;
+const appVersion = 9;
 
 class Client {
   static getDefaultSettings() {
